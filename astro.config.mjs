a6f1ab461c0d5d8feed8762df60511068327e0f5
LPuckeri4 <77989705+LPuckeri4@users.noreply.github.com>
2026-05-28 13:25:34 -0400

    redirect fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  redirects: {
+    '/surgery': '/surgery/dental-implants',
+    '/after-surgery': '/after-surgery/general-instructions',
+  },
   vite: {
     plugins: [tailwindcss()],
   },
