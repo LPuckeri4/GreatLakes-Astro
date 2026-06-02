83e3103f22f50b9b029d28ac21405dbbaf218e2e
LPuckeri4 <77989705+LPuckeri4@users.noreply.github.com>
2026-06-02 12:21:15 -0400

    schema and seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://www.greatlakessurgerycentre.com',
   output: 'static',
+  integrations: [
+    sitemap(),
+  ],
   redirects: {
     '/surgery': '/surgery/dental-implants',
     '/after-surgery': '/after-surgery/general-instructions',
